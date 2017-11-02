bd945006a5767d8bd927934d002a1b2e19a10b29
Natalie Montoya <nataliesbarron@gmail.com>
2017-11-01 21:10:32 -0600

    indent corrections

--- script.js
+++ script.js
@@ -231,116 +231,91 @@ function completeTask () {
 
 function showToDos () {  
   for (var i = 0; i < localStorage.length; i++) {
-  var retrievedObject = localStorage.getItem(localStorage.key(i));
-  var parsedObject = JSON.parse(retrievedObject);
- 
-  if (parsedObject.completed === true) {
-    var cardId = parsedObject.id;
-    $('#task-card-storage').prepend($(`#${cardId}:hidden`).show());
-    $(`#${cardId}`).closest(".task-card").toggleClass("greyed-out");
+    var retrievedObject = localStorage.getItem(localStorage.key(i));
+    var parsedObject = JSON.parse(retrievedObject);
+    if (parsedObject.completed === true) {
+      var cardId = parsedObject.id;
+      $('#task-card-storage').prepend($(`#${cardId}:hidden`).show());
+      $(`#${cardId}`).closest(".task-card").toggleClass("greyed-out");
+    }
   }
-}
 }
 
 function critical() {
   for (var i = 0; i < localStorage.length; i++) {
-  var retrievedObject = localStorage.getItem(localStorage.key(i));
-  var parsedObject = JSON.parse(retrievedObject);
-  var cardId = parsedObject.id;
-
-  if (parsedObject.quality == "Critical") {
-    $('#task-card-storage').prepend($(`#${cardId}`).show());
-    
-  } else {
-    $('#task-card-storage').prepend($(`#${cardId}`).hide());
+    var retrievedObject = localStorage.getItem(localStorage.key(i));
+    var parsedObject = JSON.parse(retrievedObject);
+    var cardId = parsedObject.id;
+    if (parsedObject.quality == "Critical") {
+      $('#task-card-storage').prepend($(`#${cardId}`).show());
+    } 
+    else {
+      $('#task-card-storage').prepend($(`#${cardId}`).hide());
+    }
   }
-}
 }
 
 function high() {
   for (var i = 0; i < localStorage.length; i++) {
-  var retrievedObject = localStorage.getItem(localStorage.key(i));
-  var parsedObject = JSON.parse(retrievedObject);
-  var cardId = parsedObject.id;
-
-  if (parsedObject.quality == "High") {
-    $('#task-card-storage').prepend($(`#${cardId}`).show());
-    
-  } else {
-    $('#task-card-storage').prepend($(`#${cardId}`).hide());
+    var retrievedObject = localStorage.getItem(localStorage.key(i));
+    var parsedObject = JSON.parse(retrievedObject);
+    var cardId = parsedObject.id;
+    if (parsedObject.quality == "High") {
+      $('#task-card-storage').prepend($(`#${cardId}`).show());
+    } 
+    else {
+      $('#task-card-storage').prepend($(`#${cardId}`).hide());
+    }
   }
-}
 }
 
 function normal() {
   for (var i = 0; i < localStorage.length; i++) {
-  var retrievedObject = localStorage.getItem(localStorage.key(i));
-  var parsedObject = JSON.parse(retrievedObject);
-  var cardId = parsedObject.id;
-
-  if (parsedObject.quality == "Normal") {
-    $('#task-card-storage').prepend($(`#${cardId}`).show());
-    
-  } else {
-    $('#task-card-storage').prepend($(`#${cardId}`).hide());
+    var retrievedObject = localStorage.getItem(localStorage.key(i));
+    var parsedObject = JSON.parse(retrievedObject);
+    var cardId = parsedObject.id;
+    if (parsedObject.quality == "Normal") {
+      $('#task-card-storage').prepend($(`#${cardId}`).show());
+    } 
+    else {
+      $('#task-card-storage').prepend($(`#${cardId}`).hide());
+    }
   }
-}
 }
 
 function low() {
   for (var i = 0; i < localStorage.length; i++) {
-  var retrievedObject = localStorage.getItem(localStorage.key(i));
-  var parsedObject = JSON.parse(retrievedObject);
-  var cardId = parsedObject.id;
-
-  if (parsedObject.quality == "Low") {
-    $('#task-card-storage').prepend($(`#${cardId}`).show());
-    
-  } else {
-    $('#task-card-storage').prepend($(`#${cardId}`).hide());
+    var retrievedObject = localStorage.getItem(localStorage.key(i));
+    var parsedObject = JSON.parse(retrievedObject);
+    var cardId = parsedObject.id;
+    if (parsedObject.quality == "Low") {
+      $('#task-card-storage').prepend($(`#${cardId}`).show());
+    } 
+    else {
+      $('#task-card-storage').prepend($(`#${cardId}`).hide());
+    }
   }
-}
 }
 
 function none() {
   for (var i = 0; i < localStorage.length; i++) {
-  var retrievedObject = localStorage.getItem(localStorage.key(i));
-  var parsedObject = JSON.parse(retrievedObject);
-  var cardId = parsedObject.id;
-
-  if (parsedObject.quality == "None") {
-    $('#task-card-storage').prepend($(`#${cardId}`).show());
-    
-  } else {
-    $('#task-card-storage').prepend($(`#${cardId}`).hide());
+    var retrievedObject = localStorage.getItem(localStorage.key(i));
+    var parsedObject = JSON.parse(retrievedObject);
+    var cardId = parsedObject.id;
+    if (parsedObject.quality == "None") {
+      $('#task-card-storage').prepend($(`#${cardId}`).show());
+    } 
+    else {
+      $('#task-card-storage').prepend($(`#${cardId}`).hide());
+    }
   }
-}
 }
 
 function moreToDos() {
-  
   var otherKeyValues = [];
   var keys = Object.keys(localStorage).slice(0, -10);
-
   for (var i = 0; i < keys.length; i++) {
     otherKeyValues.push(JSON.parse(localStorage.getItem(keys[i])));
   }
   populateExistingCards(otherKeyValues);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
